c15fb3fe8dd53355469facd811b87132c9783fc9
Kamil Jiwa <kamil.jiwa@gmail.com>
2019-02-12 02:20:45 -0800

    Add a unit test for admittance matrices and PowerWorld simulation for test system.

---
+++ test_power_system.py
@@ -0,0 +1,90 @@
+import numpy as np
+import power_system_builder
+import unittest
+
+
+class TestPowerSystem(unittest.TestCase):
+    @unittest.skip('Skip until it can be determined why load admittances are not used by PowerWorld.')
+    def test_admittance_matrix_0(self):
+        filename = 'data/data.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0)
+        ps = builder.build_system()
+
+        actual = ps.admittance_matrix_0()
+        expected = [[-10j, 6.67j, 0, 3.33j, 0, 0],
+                    [6.67j, -210j, 0, 0, 3.33j, 0],
+                    [0, 0, -205.71j, 2.38j, 0, 3.33j],
+                    [3.33j, 0, 2.38j, 12.38j, 6.67j, 0],
+                    [0, 3.33j, 0, 6.67j, -16.67j, 6.67j],
+                    [0, 0, 3.33j, 0, 6.67j, -10j]]
+
+        np.testing.assert_array_almost_equal(actual, expected, 2)
+
+    def test_admittance_matrix_1(self):
+        filename = 'data/data.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0)
+        ps = builder.build_system()
+
+        actual = ps.admittance_matrix_1()
+        expected = [[-80j, 20j, 0, 10j, 0, 0],
+                    [20j, -80j, 0, 0, 10j, 0],
+                    [0, 0, -67.14j, 7.14j, 0, 10j],
+                    [10j, 0, 7.14j, 0.96 - 37.53j, 20j, 0],
+                    [0, 10j, 0, 20j, 0.48 - 50.29j, 20j],
+                    [0, 0, 10j, 0, 20j, 1.07 - 30.39j]]
+
+        np.testing.assert_array_almost_equal(actual, expected, 2)
+
+    def test_admittance_matrix_2(self):
+        filename = 'data/data.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0)
+        ps = builder.build_system()
+
+        actual = ps.admittance_matrix_1()
+        expected = [[-80j, 20j, 0, 10j, 0, 0],
+                    [20j, -80j, 0, 0, 10j, 0],
+                    [0, 0, -67.14j, 7.14j, 0, 10j],
+                    [10j, 0, 7.14j, 0.96 - 37.53j, 20j, 0],
+                    [0, 10j, 0, 20j, 0.48 - 50.29j, 20j],
+                    [0, 0, 10j, 0, 20j, 1.07 - 30.39j]]
+
+        np.testing.assert_array_almost_equal(actual, expected, 2)
+
+    def test_admittance_matrix_0_hw04p4(self):
+        filename = 'data/sample-hw04p4.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0.01j)
+        ps = builder.build_system()
+
+        actual = ps.admittance_matrix_0()
+        expected = [[-3.33j, 1.11j, 1.11j, 1.11j],
+                    [1.11j, -22.22j, 1.11j, 0],
+                    [1.11j, 1.11j, -2.22j, 0],
+                    [1.11j, 0, 0, -1.11j]]
+
+        np.testing.assert_array_almost_equal(actual, expected, 2)
+
+    def test_admittance_matrix_1_hw04p4(self):
+        filename = 'data/sample-hw04p4.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0.01j)
+        ps = builder.build_system()
+
+        actual = ps.admittance_matrix_1()
+        expected = [[-16.67j, 3.33j, 3.33j, 3.33j],
+                    [3.33j, -13.33j, 3.33j, 0],
+                    [3.33j, 3.33j, -6.67j, 0],
+                    [3.33j, 0, 0, -3.33j]]
+
+        np.testing.assert_array_almost_equal(actual, expected, 2)
+
+    def test_admittance_matrix_2_hw04p4(self):
+        filename = 'data/sample-hw04p4.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0)
+        ps = builder.build_system()
+
+        actual = ps.admittance_matrix_2()
+        expected = [[-21.11j, 3.33j, 3.33j, 3.33j],
+                    [3.33j, -17.78j, 3.33j, 0],
+                    [3.33j, 3.33j, -6.67j, 0],
+                    [3.33j, 0, 0, -3.33j]]
+
+        np.testing.assert_array_almost_equal(actual, expected, 2)
--- test_power_system_fault.py
+++ test_power_system_fault.py
@@ -49,3 +49,66 @@ class TestPowerSystem(unittest.TestCase):
         np.testing.assert_equal(np.abs(fault.phase_current_a()), 0)
         np.testing.assert_almost_equal(np.abs(fault.phase_current_b()), 22.275, 0)
         np.testing.assert_almost_equal(np.abs(fault.phase_current_c()), 18.352, 1)
+
+    def test_3p_fault_currents_hw04p4(self):
+        filename = 'data/sample-hw04p4.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0.01j)
+        ps = builder.build_system()
+        fault = power_system_fault.FaultBuilder.build(ps, '3p', 3, 0)
+
+        np.testing.assert_almost_equal(np.abs(fault.fault_current()), 4.267, 3)
+        np.testing.assert_almost_equal(np.abs(fault.phase_current_a()), 4.267, 3)
+        np.testing.assert_almost_equal(np.abs(fault.phase_current_b()), 4.267, 3)
+        np.testing.assert_almost_equal(np.abs(fault.phase_current_c()), 4.267, 3)
+
+    def test_slg_fault_currents_hw04p4(self):
+        filename = 'data/sample-hw04p4.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0.01j)
+        ps = builder.build_system()
+        fault = power_system_fault.FaultBuilder.build(ps, 'slg', 3, 0)
+
+        np.testing.assert_almost_equal(np.abs(fault.fault_current()), 2.692, 3)
+        np.testing.assert_almost_equal(np.abs(fault.phase_current_a()), 2.692, 3)
+        np.testing.assert_equal(np.abs(fault.phase_current_b()), 0)
+        np.testing.assert_equal(np.abs(fault.phase_current_c()), 0)
+
+    def test_ll_fault_currents_hw04p4(self):
+        filename = 'data/sample-hw04p4.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0.01j)
+        ps = builder.build_system()
+        fault = power_system_fault.FaultBuilder.build(ps, 'll', 3, 0)
+
+        np.testing.assert_almost_equal(np.abs(fault.fault_current()), 3.959, 3)
+        np.testing.assert_equal(np.abs(fault.phase_current_a()), 0)
+        np.testing.assert_almost_equal(np.abs(fault.phase_current_b()), 3.959, 3)
+        np.testing.assert_almost_equal(np.abs(fault.phase_current_c()), 3.959, 3)
+
+    def test_dlg_fault_currents_hw04p4(self):
+        filename = 'data/sample-hw04p4.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0.01j)
+        ps = builder.build_system()
+        fault = power_system_fault.FaultBuilder.build(ps, 'dlg', 3, 0)
+
+        np.testing.assert_almost_equal(np.abs(fault.fault_current()), 1.772, 3)
+        np.testing.assert_equal(np.abs(fault.phase_current_a()), 0)
+        np.testing.assert_almost_equal(np.abs(fault.phase_current_b()), 4.021, 3)
+        np.testing.assert_almost_equal(np.abs(fault.phase_current_c()), 4.021, 3)
+
+    def test_3p_fault_voltages_hw04p4(self):
+        filename = 'data/sample-hw04p4.xlsx'
+        builder = power_system_builder.ExcelPowerSystemBuilder(filename, 'BusData', 'LineData', 0.01j)
+        ps = builder.build_system()
+        fault = power_system_fault.FaultBuilder.build(ps, '3p', 3, 0)
+
+        e_0 = fault.bus_voltages_0()
+        e_1 = fault.bus_voltages_1()
+        e_2 = fault.bus_voltages_2()
+
+        a = np.exp(1j * np.deg2rad(120))
+        v_0 = np.add(e_0, np.add(e_1, e_2))
+        v_1 = e_0 + np.dot(a ** 2, e_1) + np.dot(a, e_2)
+        v_2 = e_0 + np.dot(a, e_1) + np.dot(a ** 2, e_2)
+
+        np.testing.assert_array_almost_equal(np.abs(v_0), [0.66667, 0.66666, 0, 0.66667], 1)
+        np.testing.assert_array_almost_equal(np.abs(v_1), [0.66667, 0.66666, 0, 0.66667], 1)
+        np.testing.assert_array_almost_equal(np.abs(v_2), [0.66667, 0.66666, 0, 0.66667], 1)
